137cd186ac78f70752b07b9a136b36db5b9715e6
kukila <143105348+Kukilasingaram@users.noreply.github.com>
2024-04-21 09:41:26 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const API_URL = "https://gmail-backend-8ssn.onrender.comg";
 //const API_URL= "http://localhost:7000/"
-const API_URL = "https://gmail-clone-backend-t8r7.onrender.com"
+const API_URL = "https://backend-5ig2.onrender.com"
 const API_GMAIL = async (urlObject, payload, type) => {
   return await axios({
     method: urlObject.method,
